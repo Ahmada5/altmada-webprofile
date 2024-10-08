c95dcec06444e32262e5c90871ae31d1ee59b08c
ahmada5 <achmad.soleh.ahmada@gmail.com>
2024-10-08 14:27:53 +0700

    updt - HomePage for maintenance components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,45 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import {Box} from "@chakra-ui/react";
+import {Headers} from "../components/Headers.tsx";
+import {OurApps} from "../components/OurApps.tsx";
+import {Footer} from "../components/Footer.tsx";
 
 const HelloMessage: React.FC = () => {
     return (
-        <Box
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            height="100vh"
-        >
-            <Text fontSize="2xl" fontWeight="bold"> {}
-                testing awal
-            </Text>
+        <Box>
+            <Box
+                bg='tomato'
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="100vh"
+            >
+                <div>
+                    <Headers/>
+                </div>
+            </Box>
+            <Box
+                bg='pink'
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="100vh"
+            >
+                <div>
+                    <OurApps/>
+                </div>
+            </Box>
+            <Box
+                bg='green'
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="30vh"
+            >
+                <div>
+                    <Footer/>
+                </div>
+            </Box>
         </Box>
     );
 };
