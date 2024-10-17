399a3f594201a274d4e3a050475708cf62694478
ahmada5 <achmad.soleh.ahmada@gmail.com>
2024-10-17 12:06:00 +0700

    updt - add new code to call component handle svg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import {Box} from '@chakra-ui/react';
+import SvgIcon from "./common/SvgIcon";
 
 export const Footer: React.FC = () => {
     return (
         <Box>
             <h1>FOOTER</h1>
+            <SvgIcon name="Instagram" />
+            <SvgIcon name="GooglePlay" />
+            <SvgIcon name="Gmail" />
         </Box>
     )
 }
